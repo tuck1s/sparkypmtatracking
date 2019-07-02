feaaed3e3207784708509bdfde7c0ebc2f695ae4
tuck1s <steve.tuck@sparkpost.com>
2019-07-02 16:03:00 +0100

    First proper version of acct_etl

--- README.md
+++ README.md
@@ -7,5 +7,37 @@ Pre-requisites
 ## proc_acct
 Process accounting data fed by PMTA pipe - [see here](https://download.port25.com/files/UsersGuide.html#examples).
 
-## 
 
+
+
+### Installing Redis on your host
+
+Redis does not currently seem to be available via a package manager on EC2 Linux.
+
+Follow the QuickStart guide [here](https://redis.io/topics/quickstart), following the "Installing Redis" steps
+and "Installing Redis more properly" steps. EC2 Linux does not have the
+update-rc.d command, use `sudo chkconfig redis_6379 on` instead.
+
+Here's the steps I followed:
+```
+# Building
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make install
+
+# install "properly" as a service
+sudo vi /etc/redis/6379.conf
+sudo cp utils/redis_init_script /etc/init.d/redis_6379
+sudo vi /etc/init.d/redis_6379
+sudo cp redis.conf /etc/redis/6379.conf
+sudo mkdir /var/redis/6379
+sudo vim /etc/redis/6379.conf
+
+sudo chkconfig redis_6379 on
+sudo /etc/init.d/redis_6379 start
+
+# test
+redis-cli ping
+```
--- acct_etl/acct_etl.go
+++ acct_etl/acct_etl.go
@@ -2,20 +2,74 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/smartystreets/scanners/csv"
 )
 
-func main() {
-	logfile, err := os.OpenFile("acct_etl.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatalln("Failed to open log file", logfile, ":", err)
+func check(e error) {
+	if e != nil {
+		console_and_log_fatal(e)
 	}
+}
+
+func console_and_log_fatal(s ...interface{}) {
+	fmt.Println(s...)
+	log.Fatalln(s...)
+}
+
+func main() {
+	flag.Parse()
+	var f *os.File
+
+	// Use logging, as this program will be executed without an attached console
+	logfile, err := os.OpenFile("acct_etl.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	check(err)
 	log.SetOutput(logfile)
 
-	input := bufio.NewScanner(os.Stdin)
+	// Check if input file specified on command-line args, or using stdin
+	switch flag.NArg() {
+	case 0:
+		f = os.Stdin
+		break
+	case 1:
+		f, err = os.Open(flag.Arg(0))
+		check(err)
+		break
+	default:
+		console_and_log_fatal("Command line args: input must be from stdin or file")
+	}
+	inbuf := bufio.NewReader(f)
+
+	// Prepare to load records into Redis. Assume server is on the standard port
+	client := redis.NewClient(&redis.Options{
+		Addr:     ":6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	_, err = client.Ping().Result()
+	check(err)
+
+	const ttl = time.Duration(time.Hour * 24 * 10)
+	const expectedLen = 5
+	// Scan input records - expected format recType, fromAddr, RcptTo, message_id, tracking_id
+	input := csv.NewScanner(inbuf)
 	for input.Scan() {
-		ln := input.Text()
-		log.Println(ln)
+		r := input.Record()
+		if len(r) != expectedLen {
+			console_and_log_fatal("Accounting record not in expected format - should have", expectedLen, "elements")
+		}
+		recType, fromAddr, RcptTo, message_id, tracking_id := r[0], r[1], r[2], r[3], r[4]
+		if recType != "d" {
+			console_and_log_fatal("Accounting record not of type d")
+		}
+		_, err := client.Set("trk_"+tracking_id, message_id, ttl).Result()
+		check(err)
+		log.Println("Loaded", tracking_id, "=", message_id, "into Redis, from=", fromAddr, "RcptTo=", RcptTo)
 	}
 }
