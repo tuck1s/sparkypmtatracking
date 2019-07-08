76ccf045d10c8659c8960766dce45e5b02b2ff82
tuck1s <steve.tuck@sparkpost.com>
2019-07-08 18:00:11 +0100

    buffered i/o in feeder

--- src/feeder/feeder.go
+++ src/feeder/feeder.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
 	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	. "github.com/sparkyPmtaTracking/src/common"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -16,20 +21,31 @@ import (
 )
 
 // Make a SparkPost formatted unique event_id, which needs to be a decimal string 0 .. (2^63-1)
-func uniq_event_id() string {
+func uniqEventId() string {
 	u := uuid.New()
 	num := binary.LittleEndian.Uint64(u[:8]) & 0x7fffffffffffffff
 	return strconv.FormatUint(num, 10)
 }
 
-// Map our own tracking ID to the SparkPost Message ID via Redis
+// Make a SparkPost formatted unique message_id, which needs to be a hex string
+func uniqMessageId() string {
+	u := uuid.New()
+	h := hex.EncodeToString(u[:12])
+	return strings.ToUpper(h)
+}
+
+// Map our own tracking ID to the SparkPost Message ID via Redis - if found
 func trackingIDToMessageID(eStr string, client *redis.Client) string {
 	var t TrackEvent
 	err := json.Unmarshal([]byte(eStr), &t)
 	Check(err)
 	//t.TrackingID = "a5a1050437e345389bc2c7d6d79743f5"			//TODO: debug
-	msgID, err := client.Get("trk_" + t.TrackingID).Result()
-	Check(err)
+	trkID := "trk_" + t.TrackingID
+	msgID, err := client.Get(trkID).Result()
+	if err == redis.Nil {
+		log.Println("Tracking ID", trkID, "not found - using a generated unique message_id")
+		msgID = uniqMessageId()
+	}
 	return msgID
 }
 
@@ -37,8 +53,11 @@ func trackingIDToMessageID(eStr string, client *redis.Client) string {
 const ingestBatchSize = 10000
 const ingestMaxWait = 10 * time.Second
 
-// Because the JSON declarations coincide, we can unmarshal a TrackingEvent into part of a SparkPostEvent
+// Prepare a batch of TrackingEvents for ingest to SparkPost.
+// Because the JSON declarations coincide, we can unmarshal a TrackingEvent into the leaf part of a SparkPostEvent
 func sparkPostIngest(batch []string, client *redis.Client) {
+	var ingestData bytes.Buffer
+	ingestData.Grow(1024 * len(batch)) // Preallocate approx size of the string for efficiency
 	for _, eStr := range batch {
 		var e SparkPostEvent
 		eptr := &e.EventWrapper.EventGrouping
@@ -55,7 +74,7 @@ func sparkPostIngest(batch []string, client *redis.Client) {
 			Zip:        21046,
 			PostalCode: "21046",
 		}
-		eptr.EventID = uniq_event_id()
+		eptr.EventID = uniqEventId()
 		eptr.InitialPixel = false
 		eptr.MessageID = trackingIDToMessageID(eStr, client)
 		eptr.RoutingDomain = strings.Split(eptr.RcptTo, "@")[1]
@@ -63,8 +82,23 @@ func sparkPostIngest(batch []string, client *redis.Client) {
 		eptr.OpenTracking = true
 		s, err := json.Marshal(e)
 		Check(err)
-		fmt.Println(string(s))
+		ingestData.Write(s)
+		ingestData.WriteString("\n")
 	}
+	// Now got ingestData in buffer - flow through gzip writer
+	var zbuf bytes.Buffer
+	ir := bufio.NewReader(&ingestData)
+	zw := gzip.NewWriter(&zbuf)
+	_, err := io.Copy(zw, ir)
+	Check(err)
+	err = zw.Close()
+	Check(err)
+	//
+	zr := bufio.NewReader(&zbuf)
+	hd := hex.Dumper(os.Stdout)
+	_, err = io.Copy(hd, zr)
+	Check(err)
+
 }
 
 func main() {
