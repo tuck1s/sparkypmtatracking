8cf80426e5bd92f9b1363e814fdef93b3318f3c0
tuck1s <steve.tuck@sparkpost.com>
2019-10-29 10:36:15 +0000

    Startup scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-go build ./cmd/acct_etl
-go build ./cmd/feeder
-go build ./cmd/tracker
-go build ./cmd/wrapper
+go build -v ./cmd/acct_etl
+go build -v ./cmd/feeder
+go build -v ./cmd/tracker
+go build -v ./cmd/wrapper
--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 . setenvs.sh
-~/go/src/github.com/sparkyPmtaTracking/src/tracker/tracker &
-~/go/src/github.com/sparkyPmtaTracking/src/feeder/feeder &
-~/go/src/github.com/sparkyPmtaTracking/src/feeder/wrapper -in_hostport :5587 -out_hostport pmta.signalsdemo.trymsys.net:587 -privkeyfile privkey.pem -certfile fullchain.pem -downstream_debug debug_downstream.log -upstream_data_debug debug_upstream.eml --insecure_skip_verify --verbose &
+cmd/tracker/tracker &
+cmd/feeder/feeder &
+cmd/wrapper/wrapper -in_hostport :5587 -out_hostport pmta.signalsdemo.trymsys.net:587 \
+ -privkeyfile privkey.pem -certfile fullchain.pem \
+ -engagement_url http://smtp.proxy.trymsys.net \
+ -logfile wrapper.log -verbose \
+ -downstream_debug debug_downstream.log -upstream_data_debug debug_upstream.eml \
+ -insecure_skip_verify
