72c77a74eed2e7671ac9a25de113f71db4ddbd9d
tuck1s <steve.tuck@sparkpost.com>
2020-01-24 21:51:55 +0000

    remove obsolete shell script

--- events_1k.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..1000}
-do
-curl -v http://localhost:8888/tracking/open/eJxdT81uwyAMfpWI6xqSbolYeuoDrKc9AHKIoawBInCqRlXffVBt0jT5Ytnf750pcAtY46Wd2KFiH5CoOlm_ElafcLXeJLarWFQLSQoFQZjopW3bYS-G_ugD1SGCN8h15GNYvdrqZP2FU9xc2hL3SEXBJSN1DO5X4pgIr0h12f9jn24OCTL4_siHtI5fqKhw_4QiiAZJztlNrnEu3zPRcmgavOVWM3IVXGP9hDd-Jjc_ORHUJQv81N0LGF_LdAI6_T5B1-Nbr4dOTEKrHtnjG0smYWM=
-done
