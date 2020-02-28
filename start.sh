4848170d682e3c40fd041f91693dc5b6b19fe893
tuck1s <steve.tuck@sparkpost.com>
2020-02-28 22:07:45 +0000

    https changes

--- start.sh
+++ start.sh
@@ -8,14 +8,17 @@
 
 # needs to be started as root if in_hostport is in range 1..1024
 sudo ./wrapper -in_hostport :587 -out_hostport :5587 \
- -privkeyfile trymsys.net.key -certfile trymsys.net.crt \
+ -privkeyfile /etc/pki/tls/private/trymsys.net.key \
+ -certfile /etc/pki/tls/certs/trymsys.net.crt \
  -logfile wrapper.log \
  -insecure_skip_verify \
- -tracking_url http://pmta.signalsdemo.trymsys.net \
+ -tracking_url https://pmta-signalsdemo.trymsys.net \
  -track_open -track_initial_open -track_click &
-
  # -verbose \
  # -downstream_debug debug_downstream.log -upstream_data_debug debug_upstream.eml \
+
+# Note using https: tracking, a standard wildcard cert only permits one level of subdomain, as per
+# https://www.digicert.com/kb/ssl-support/wildcard-san-names.htm
 
 # acct_etl is run directly by PowerMTA - refer to README.md for how to set this up
 sudo service pmta stop
